8daf5ca6c576d050fd5d46087d85b3853ff81345
Oscar Fraxedas <fraxedas@gmail.com>
2022-10-18 21:42:01 -0400

    Recreate the project

--- src/server/config.js
+++
@@ -1,8 +0,0 @@
-const env = process.env;
-
-export default {
-  port: env.PORT || 1234,
-  host: env.HOST || 'localhost',
-  isDev: env.NODE_ENV !== 'production',
-  isBrowser: typeof window !== 'undefined',
-};
